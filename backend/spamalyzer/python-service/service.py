c855cedc7f630f609661776bd8f658b5eca2fa97
DiFortis <difortis3@gmail.com>
2025-01-19 14:21:27 +0100

    changed listening to 0.0.0.0

--- backend/spamalyzer/python-service/service.py
+++ backend/spamalyzer/python-service/service.py
@@ -56,4 +56,4 @@ if __name__ == '__main__':
         MODEL = pickle.load(f)
         print("Model loaded!")
 
-    app.run(host='127.0.0.1', port=9243, debug=True)
+    app.run(host='0.0.0.0', port=9243, debug=True)
